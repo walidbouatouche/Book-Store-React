cbc94b878f918fe5d9503a9d48877f60503416f0
Your Name <walid.info27@gmail.com>
2020-08-25 15:55:22 +0200

    prepare backend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+﻿require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const errorHandler = require('_middleware/error-handler');
+const db = require('_helpers/db');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+app.listen(port, () => console.log('Server listening on port ' + port));
