34d78431ee393db4c0b24aa424323c7f906a8373
Your Name <walid.info27@gmail.com>
2020-08-27 15:55:00 +0200

    book table and favorate table amazon

--- backend/_models/book.model.js
+++ backend/_models/book.model.js
@@ -9,8 +9,9 @@ const bookSchema = mongoose.Schema({
     author: { type: String, required: true },
     publishedAt: { type: String, required: true },
     addBy: { type: String, required: true },
-    createdAt: { type: String, required: true },
     rate: { type: Number, required: true },
+    AmazonLink: { type: String, required: true },
+    createdAt: { type: String, required: true },
     updatedAt: { type: String, required: true }
 });
 
